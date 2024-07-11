7981ce4f2aba63f1a5d587123f6d4333f3fa8ba6
FKshnik <kam7dom@gmail.com>
2024-07-11 18:21:24 +0300

    move from sleep to additionalMsec

--- src/hack.ts
+++ src/hack.ts
@@ -2,6 +2,5 @@ import { NS } from "@ns"
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    await ns.sleep(ns.args[1] as number)
-    await ns.hack(ns.args[0] as string)
+    await ns.hack(ns.args[0] as string, { additionalMsec: ns.args[1] as number })
 }
